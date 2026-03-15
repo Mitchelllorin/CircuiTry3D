4871ab7de125f91a653af58a81a942da068de67e
Mitchelllorin <Mitchelllorin@users.noreply.github.com>
2026-03-15 19:21:53 +0000

    deploy: 706be007e50833e3dd381639b2685a2b5f6cfb3e

---
+++ assets/web-MX3Id_r2.js
@@ -0,0 +1,1 @@
+import{x as n}from"./index-BpIg0FX6.js";import"./router-BZzXuVii.js";import"./react-vendor-Bzgz95E1.js";class t extends n{async show(e){}async hide(e){}}export{t as SplashScreenWeb};
