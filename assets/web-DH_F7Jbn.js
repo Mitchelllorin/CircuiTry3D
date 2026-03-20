39cb2a7f1a1eaf109225faee98211c7c46808254
Mitchelllorin <Mitchelllorin@users.noreply.github.com>
2026-03-20 19:04:06 +0000

    deploy: 52f65d930d69bc22fb565f940e23a431e527997d

---
+++ assets/web-DH_F7Jbn.js
@@ -0,0 +1,1 @@
+import{z as n}from"./index-i2Vbx_o0.js";import"./router-BZzXuVii.js";import"./react-vendor-Bzgz95E1.js";class t extends n{async show(e){}async hide(e){}}export{t as SplashScreenWeb};
